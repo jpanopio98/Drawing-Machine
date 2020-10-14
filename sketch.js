0f3605ed8d5abdce1e9f818bd9c3f7e681b06251
Jedd Panopio <37092739+JeddPanopio@users.noreply.github.com>
2020-10-14 04:25:20 -0700

    3.0-3.6 videos

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+
+
+let array = [];
+
+
+function setup() {
+  createCanvas(600, 600);
+  background(200);
+  strokeWeight(5);
+  noFill();
+}
+
+function draw() {
+  if (mouseIsPressed == true) {
+
+    //line(mouseX, mouseY,pmouseX,pmouseY);
+    array.push([mouseX,mouseY])
+  }
+}
+
+function keyTyped(){
+
+
+  if(key == 's'){
+    //saves image
+    saveCanvas('filename', 'png');
+  }else if (key == 'd') {
+    //displays image
+
+beginShape();
+    for (let i = 0;i < array.length; i++) {
+      //line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1]);
+    }
+endShape();
+  }
+
+  return false;
+}
